dbe272663baa8ef6a9c05ad235ceddf0269edc9a
lokeshchintha <20a91a05e6@aec.edu.in>
2023-08-16 17:52:33 +0530

    react-app

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  const loadUsers = async () => {
+    const response = await fetch("https://api.github.com/users");
+    const jsonResponse = await response.json();
+    setUsers(jsonResponse);
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello All</h1>
+      <button onClick={loadUsers}>Get Data</button>
+
+      <h2>Users:</h2>
+      <ul>
+        {users.map(({ id, login, avatar_url }) => (
+          <li key={id}>
+            Name: {login} Avator: {avatar_url}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
+
